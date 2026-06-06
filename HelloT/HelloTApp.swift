840dd78c55c6482352fe9fe228ec1d8e18964c49
Jasper <krieg@sina.com>
2026-06-07 01:23:39 +0800

    Initial Commit

---
+++ HelloT/HelloTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTApp.swift
+//  HelloT
+//
+//  Created by Jingyu Du on 2026/6/7.
+//
+
+import SwiftUI
+
+@main
+struct HelloTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloTTests/HelloTTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTTests.swift
+//  HelloTTests
+//
+//  Created by Jingyu Du on 2026/6/7.
+//
+
+import Testing
+@testable import HelloT
+
+struct HelloTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
